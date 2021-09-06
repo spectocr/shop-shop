8daf7b550c57ceaabc4b1b474ffb2db11354d574
spectocr <highstakes22@gmail.com>
2021-09-06 12:46:14 -0400

    finishing 22.1

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext; //Every Context object comes with two components, a Provider and Consumer. The Provider is a special type of React component that we wrap our application in so it can make the state data that's passed into it as a prop available to all other components. The Consumer is our means of grabbing and using the data that the Provider holds for us.
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+      products: [],
+      categories: [],
+      currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+  };
+
+  const useStoreContext = () => {
+    return useContext(StoreContext);
+  };
+
+  export { StoreProvider, useStoreContext };
